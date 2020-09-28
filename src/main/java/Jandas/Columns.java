2ab3ebbd786420ef21d830457059f14a48a0e093
SVAX29 <s.vanin@outlook.it>
2020-09-28 11:17:23 +0200

    Columns - set values as immutable any kind of list

--- src/main/java/Jandas/Columns.java
+++ src/main/java/Jandas/Columns.java
@@ -1,4 +1,19 @@
 package Jandas;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class Columns {
+
+    private String _name;
+    private List _values;
+    private Object _type;
+
+    public <T> void setValues(List<T> values) {
+        this._values = List.copyOf(values);;
+    }
 }
